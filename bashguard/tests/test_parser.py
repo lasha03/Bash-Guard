fe2d0dd0d10e6dca64431e367878746b8245dc71
GiorgiKobakhia <gkoba21@freeuni.edu.ge>
2025-04-21 06:57:31 +0400

    basic parser test

---
+++ bashguard/tests/test_parser.py
@@ -0,0 +1,15 @@
+import os
+from bashguard.core.parser import Parser
+
+def test_get_variables():
+    
+    content = ""
+    test_file_path = os.path.join(os.path.dirname(__file__), 'test.sh')
+    with open(test_file_path, 'r') as f:
+        content = f.read()
+
+    p = Parser(content)
+    p.parse()
+    res = p.get_variables()[0]
+    assert res.kind == 'assignment'
+    assert res.word == 'PATH=/usr/bin'
