84c7d47581ae1495806d7eb27bfb30d7aa67148d
GiorgiKobakhia <gkoba21@freeuni.edu.ge>
2025-04-21 04:09:33 +0400

    basic parser which returns list of declared variables

---
+++ bashguard/core/parser.py
@@ -0,0 +1,43 @@
+"""
+Parser for content analysis, based on bashlex parser.
+"""
+
+from typing import List
+import bashlex
+
+class Parser:
+    """
+    """
+    
+    def __init__(self, content: str):
+        """
+        Initialize the parser.
+        
+        Args:
+            content: Content to analyze
+        """
+        self.content = content
+        self.assignment_nodes = []
+
+    def parse(self):
+        """Parse content using bashlex and divide according to keywords"""
+        self.parts = bashlex.parse(self.content)
+        
+        for node in self.parts:
+            self.__divide(node)
+
+        
+    def __divide(self, node: bashlex.ast.node):
+        
+        if hasattr(node, 'parts') == False or len(node.parts) == 0:
+            if node.kind == 'assignment':
+                self.assignment_nodes.append(node)
+            return
+
+        for child_node in node.parts:
+            self.__divide(child_node)
+
+
+    def get_variables(self) -> List[bashlex.ast.node]:
+        return self.assignment_nodes
+        
