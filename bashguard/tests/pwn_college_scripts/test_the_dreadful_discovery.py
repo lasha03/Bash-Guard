75c6dacc0efdca4191b3d1195b0559afa38ff98b
GiorgiKobakhia <gkoba21@freeuni.edu.ge>
2025-05-31 05:17:22 +0400

    add some tests

---
+++ bashguard/tests/pwn_college_scripts/test_the_dreadful_discovery.py
@@ -0,0 +1,22 @@
+import os
+
+from pathlib import Path
+from bashguard.analyzers import ScriptAnalyzer
+from bashguard.core import Description
+
+
+def test_the_dreadful_discovery():
+    
+    test_file_path = os.path.join(os.path.dirname(__file__), 'test_the_dreadful_discovery.sh')
+    
+    analyzer = ScriptAnalyzer(test_file_path)
+    
+    vulnerabilities = analyzer.analyze()
+    for vuln in vulnerabilities:
+        print(vuln)
+
+    assert len(vulnerabilities) == 2
+    assert vulnerabilities[0].description == Description.COMMAND_INJECTION 
+    assert vulnerabilities[1].description == Description.COMMAND_INJECTION
+
+test_the_dreadful_discovery()
---
+++ bashguard/tests/pwn_college_scripts/test_way_of_the_wildcard.py
@@ -0,0 +1,22 @@
+import os
+
+from pathlib import Path
+from bashguard.analyzers import ScriptAnalyzer
+from bashguard.core import Description
+
+
+def test_way_of_the_wildcard():
+    
+    test_file_path = os.path.join(os.path.dirname(__file__), 'test_way_of_the_wildcard.sh')
+    
+    analyzer = ScriptAnalyzer(test_file_path)
+    
+    vulnerabilities = analyzer.analyze()
+    for vuln in vulnerabilities:
+        print(vuln)
+
+    assert len(vulnerabilities) == 0
+    # assert vulnerabilities[0].description == Description.VARIABLE_EXPANSION 
+    
+
+test_way_of_the_wildcard()
