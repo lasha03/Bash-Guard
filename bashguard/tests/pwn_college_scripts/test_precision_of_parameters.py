2025769cdf465e65d9877a4feaa700d5a94878e3
GiorgiKobakhia <gkoba21@freeuni.edu.ge>
2025-06-05 22:38:35 +0400

    add more tests

---
+++ bashguard/tests/pwn_college_scripts/test_precision_of_parameters.py
@@ -0,0 +1,24 @@
+import os
+
+from pathlib import Path
+from bashguard.analyzers import ScriptAnalyzer
+from bashguard.core import VulnerabilityType
+
+
+def test_precision_of_parameters():
+
+    test_file_path = os.path.join(os.path.dirname(__file__), 'test_precision_of_parameters.sh')
+
+    analyzer = ScriptAnalyzer(test_file_path)
+    
+    vulnerabilities = analyzer.analyze()
+    for vuln in vulnerabilities:
+        print(vuln)
+
+    assert len(vulnerabilities) == 4
+    assert vulnerabilities[0].vulnerability_type == VulnerabilityType.VARIABLE_EXPANSION
+    assert vulnerabilities[1].vulnerability_type == VulnerabilityType.VARIABLE_EXPANSION
+    assert vulnerabilities[2].vulnerability_type == VulnerabilityType.VARIABLE_EXPANSION
+    assert vulnerabilities[3].vulnerability_type == VulnerabilityType.COMMAND_INJECTION
+
+test_precision_of_parameters()
